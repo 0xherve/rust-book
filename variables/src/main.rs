6b5dd273b09e18783505c0025a5056aba4284180
Eminence <imenarwigema@gmail.com>
2025-09-30 00:34:15 +0200

    reading arrays

--- variables/src/main.rs
+++ variables/src/main.rs
@@ -1,7 +1,16 @@
+use std::io;
+
 fn main () {
-    let array = [2;5];
-    
-    for i in 0..5 {
-        println!("The number {} component is: {}", i, array[i]);
-    }
+    let array = [10, 11, 12, 13, 14, 15];
+
+    let mut index = String::new();
+    println!("Please enter the index you want to see between 0 and 4");
+
+    io::stdin()
+    .read_line(&mut index)
+    .expect("Failed to read the line");
+
+    let index:usize = index.trim().parse().expect("Please enter a number");
+
+    println!("\nThe number at {index} is {}", array[index]);
 }
