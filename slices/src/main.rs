3e1358482df45f2befed7a9e23bc6b1d33c3de78
Eminence <imenarwigema@gmail.com>
2025-10-06 13:11:48 +0200

    slices in rust

--- slices/src/main.rs
+++ slices/src/main.rs
@@ -1,3 +1,18 @@
 fn main() {
-    println!("Hello, world!");
+    let mut s = String::from("hello world");
+
+    let first_word = get_first(&s);
+    s.clear();
+
+    println!("The first word is: {first_word}");
+
+}
+
+fn get_first(s: &String) -> &str{
+    for (i, &item) in s.as_bytes().iter().enumerate() {
+        if item == b' ' {
+            return &s[..i];
+        }
+    }
+    &s[..]
 }
